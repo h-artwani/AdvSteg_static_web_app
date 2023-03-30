4f3ca65fedef04f065a91a6c11b3997798bbd37d
Harsh Artwani <harshartwani12@gmail.com>
2023-03-29 21:02:44 -0500

    HA: Added authentication.

--- readme.md
+++ readme.md
@@ -1,7 +1,23 @@
-# Vanilla JavaScript App
+# Advanced Steganography web app
 
-[Azure Static Web Apps](https://docs.microsoft.com/azure/static-web-apps/overview) allows you to easily build JavaScript apps in minutes. Use this repo with the [quickstart](https://docs.microsoft.com/azure/static-web-apps/getting-started?tabs=vanilla-javascript) to build and customize a new static site.
+## Setup & Installation
 
-This repo is used as a starter for a _very basic_ HTML web application using no front-end frameworks.
+Make sure you have the latest version of Python installed.
 
-This repo has a dev container. This means if you open it inside a [GitHub Codespace](https://github.com/features/codespaces), or using [VS Code with the remote containers extension](https://code.visualstudio.com/docs/remote/containers), it will be opened inside a container with all the dependencies already installed.
+```bash
+git clone <repo-url>
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+## Running The App
+
+```bash
+python main.py
+```
+
+## Viewing The App
+
+Go to `http://127.0.0.1:5000`
---
+++ website/views.py
@@ -0,0 +1,100 @@
+from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user
+import json
+from .steg_advanced import encode_img, decode_file
+import os
+import cv2
+
+views = Blueprint('views', __name__)
+
+UPLOAD_FOLDER = "website/static/"
+
+# with open(UPLOAD_FOLDER + "data.json") as json_file:
+#     store_dict = json.load(json_file)
+
+store_dict = {}
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    return render_template("home.html", user=current_user)
+
+@views.route('/method' , methods = ['GET', 'POST'])
+def method():
+    encoded_images_list= os.listdir(UPLOAD_FOLDER + "encoded_images")
+    method = request.form['method']
+    user = current_user
+    if method == "encode":
+        return render_template('encode.html', user=user)
+    else:
+        return render_template('decode.html', user=user, list_files=encoded_images_list)
+
+@views.route('/encode' , methods = ['GET', 'POST'])
+def encode():
+    request_method = request.method
+    if request_method == 'POST':
+        method = request.form['method']
+        if method == "encode":
+            file_to_encode = request.files['file_to_encode']
+            image = request.files['image']
+            lth_bit = int(request.form['L'])
+            s_bit = int(request.form['S'])
+            return name(method=method, file_to_encode=file_to_encode, image=image, lth_bit=lth_bit, s_bit=s_bit)
+    return render_template('encode.html', request_method = request_method, user=current_user)
+
+@views.route('/decode' , methods = ['GET', 'POST'])
+def decode():
+    request_method = request.method
+    if request_method == 'GET':
+        ini_list = request.args.get('list_files')
+        ini_list_files = ini_list.strip('][').split(', ')
+        list_files = []
+        for name_list in ini_list_files:
+            list_files.append(name_list[1:len(name_list)-1])
+    if request_method == 'POST':
+        method = request.form['method']
+        if method == "decode":
+            encoded_name = request.form['encoded_name']
+            return name(method=method, file_to_encode="", image=encoded_name, lth_bit=0, s_bit=0)
+    return render_template('decode.html', request_method = request_method, list_files=list_files)
+
+def name(method, file_to_encode, image, lth_bit, s_bit):
+    if method == "encode":
+        temp_list = []
+        image_name = image.filename
+        image.save(os.path.join(UPLOAD_FOLDER + 'uploaded_images_for_encode', image_name))
+        file_to_encode.save(os.path.join(UPLOAD_FOLDER + 'uploaded_files_to_encode', file_to_encode.filename))
+        input_image = UPLOAD_FOLDER + "uploaded_images_for_encode/" + image_name
+        file_to_encode = UPLOAD_FOLDER + "uploaded_files_to_encode/" + file_to_encode.filename
+        # split the absolute path and the file
+        path, file = os.path.split(input_image)
+        # split the filename and the image extension
+        filename, ext = file.split(".")
+        output_image = "./website/static/encoded_images/"+f"{filename}_encoded.{ext}"
+        print(output_image)
+        temp_list.append(lth_bit)
+        temp_list.append(s_bit)
+        store_dict[f"{filename}_encoded.{ext}"] = temp_list
+        # Serializing json
+        json_object = json.dumps(store_dict, indent=4)
+        # Writing to data.json
+        with open(UPLOAD_FOLDER + "data.json", "w") as outfile:
+            outfile.write(json_object)
+        encoded_image = encode_img(image_name=input_image, secret_data=file_to_encode, lth_bit=lth_bit, s_bit=s_bit)
+        cv2.imwrite(output_image, encoded_image)
+        print("[+] Saved encoded image.")
+        output_image = output_image[9:]
+        return render_template('encoded_image_display.html', user_image=output_image, user= current_user)
+
+    elif method == "decode":
+        with open(UPLOAD_FOLDER + "data.json") as json_file:
+            data = json.load(json_file)
+        temp_list = data[image]
+        lth_bit = temp_list[0]
+        s_bit = temp_list[1]
+        decoded_data = decode_file(UPLOAD_FOLDER + 'encoded_images/' + image, lth_bit=lth_bit, s_bit=s_bit)
+        print(decoded_data)
+        final = decoded_data[7:]
+        print(final)
+
+        return render_template('decoded_file_display.html', filename=final, user= current_user)
